04e59804d42786a0a882c6abff858ec306bddcf7
zhanglufen <zhanglufen@9fbank.com.cn>
2020-01-22 17:27:09 +0800

    add: state and lifecycle, event.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,23 @@
 import React from 'react'
 import './index.scss'
 
-export default function Button() {
-  return <button className="button">按钮</button>
+export default class Button extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { status: false }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick() {
+    const val = !this.state.status
+    this.setState({
+      status: val,
+    })
+  }
+  render() {
+    return (
+      <button className="button" onClick={this.handleClick}>
+        {this.state.status ? 'ON' : 'OFF'}
+      </button>
+    )
+  }
 }
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 import './index.scss'
 
+import Clock from '../../components/Clock'
+import Button from '../../components/Button'
+
 export default function Home(props) {
   let location = useLocation()
   console.log(location.pathname, 'home')
 
   return (
     <div className="Home">
-      <h3>Hello {props['title']}!</h3>
+      <Clock></Clock>
+      <h3>{props['title']}</h3>
+      <Button></Button>
     </div>
   )
 }
