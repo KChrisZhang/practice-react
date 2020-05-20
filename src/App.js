d49ae65f8d01b9d46b2498b8737d86c499167974
KChrisZhang <zhanglufen@9fbank.com.cn>
2020-05-20 11:20:46 +0800

    添加 404, 路由重定向

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
+import { Switch, Route, Link, Redirect } from 'react-router-dom'
 import './App.css'
 
 import Home from './views/Home'
@@ -9,6 +9,7 @@ import Counter from './views/Counter'
 import TodoApp from './views/TodoApp'
 import List from './views/List'
 import Detail from './views/Detail'
+import Error from './views/404'
 
 function App() {
   return (
@@ -34,7 +35,11 @@ function App() {
         </li>
       </ul>
 
+      {/* 一般重定向需要搭配Switch一起使用 */}
+      {/* Switch 如果匹配到了路由，就不会再往下匹配了；如果不写Switch，则一直会匹配到404页面 */}
       <Switch>
+        {/* 如果访问User页面，则重定向到List页面 */}
+        <Redirect from="/user" to="list"></Redirect>
         <Route exact path="/">
           <Home title="首页"></Home>
         </Route>
@@ -52,6 +57,8 @@ function App() {
         </Route>
         <Route path="/list" component={List}></Route>
         <Route path="/detail/:id" component={Detail}></Route>
+        {/* 没有写path表示匹配到所有路径 */}
+        <Route component={Error}></Route>
       </Switch>
     </div>
   )
