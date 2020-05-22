27d07455b4fff638866e6f94c8ba9c669066c562
KChrisZhang <zhanglufen@9fbank.com.cn>
2020-05-22 10:01:43 +0800

    修复 按需加载页面闪屏问题

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,31 @@
 import React from 'react'
 
-const Home = React.lazy(() => import(/*webpackchunkname:Home*/ '../views/Home'))
-const User = React.lazy(() => import(/*webpackchunkname:User*/ '../views/User'))
-const Login = React.lazy(() => import('../views/Login'))
-const Counter = React.lazy(() => import('../views/Counter'))
-const TodoApp = React.lazy(() => import('../views/TodoApp'))
-const List = React.lazy(() => import('../views/List'))
-const Detail = React.lazy(() => import('../views/Detail'))
-const Error = React.lazy(() => import('../views/404'))
+/**
+ * 按需加载，页面实现闪屏问题
+ */
+// const Home = React.lazy(() => import(/*webpackchunkname:Home*/ '../views/Home'))
+// const User = React.lazy(() => import(/*webpackchunkname:User*/ '../views/User'))
+// const Login = React.lazy(() => import('../views/Login'))
+// const Counter = React.lazy(() => import('../views/Counter'))
+// const TodoApp = React.lazy(() => import('../views/TodoApp'))
+// const List = React.lazy(() => import('../views/List'))
+// const Detail = React.lazy(() => import('../views/Detail'))
+// const Error = React.lazy(() => import('../views/404'))
 
-const About = React.lazy(() => import('../views/User/About'))
-const Setting = React.lazy(() => import('../views/User/Setting'))
+// const About = React.lazy(() => import('../views/User/About'))
+// const Setting = React.lazy(() => import('../views/User/Setting'))
+
+import Home from '../views/Home'
+import User from '../views/User'
+import Login from '../views/Login'
+import Counter from '../views/Counter'
+import TodoApp from '../views/TodoApp'
+import List from '../views/List'
+import Detail from '../views/Detail'
+import Error from '../views/404'
+
+import About from '../views/User/About'
+import Setting from '../views/User/Setting'
 
 export default [
   {
